ae9d5f48605e6ee4914e44cf6469f11a5212d7a1
Federico Stragliati <f.stragliati@usal.edu.ar>
2024-08-14 17:44:46 -0300

    v1.3

--- src/dao/implementaciones/ClienteDAOImpMySQL.java
+++ src/dao/implementaciones/ClienteDAOImpMySQL.java
@@ -2,6 +2,7 @@ package dao.implementaciones;
 
 import dao.interfaces.IClienteDAO;
 import dominio.Cliente;
+import shared.ConnectionSQL;
 
 import java.io.IOException;
 import java.sql.SQLException;
@@ -9,8 +10,27 @@ import java.util.Collections;
 import java.util.List;
 
 public class ClienteDAOImpMySQL implements IClienteDAO {
+
+    private ConnectionSQL sql;
+    private String addSt;
+    private String getSt;
+    private String updateSt;
+    private String deleteSt;
+    private String selectAllSt;
+
+    public ClienteDAOImpMySQL() {
+        sql = new ConnectionSQL();
+        addSt = "insert into cliente (CUIT, Nombre, Email, Telefono, Activo) values (?,?,?,?,?)"; // unico correcto
+        getSt = "select * from Camiones where dominio = ?;";
+        updateSt = "update Camiones set marca = ?, modelo = ?, dominio = ?, capacidad = ?," +
+                "tanque = ?, consumo = ?  where idCamion = ?;";
+        deleteSt = "delete from Camiones where idCamion = ?;";
+        selectAllSt = "select * from Camiones";
+    }
+
     @Override
     public boolean createCliente(Cliente c) throws SQLException, ClassNotFoundException, IOException {
+
         return false;
     }
 
